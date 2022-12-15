902e7f2bf68e05cf10d061a6a57f408eb03333ab
Bdkoder <bdkoder360@gmail.com>
2022-12-15 17:13:00 +0600

    setup onesignla & add registration & login screen

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by mac on 11/12/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
